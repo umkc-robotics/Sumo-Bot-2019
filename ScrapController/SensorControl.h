53f608b7ee9ccf9be628ebad5c3b3d9286f988df
Michael Essmyer <mde9rb@mail.umkc.edu>
2019-04-06 12:22:27 -0500

    Update SensorControl.h

--- ScrapController/SensorControl.h
+++ ScrapController/SensorControl.h
@@ -18,16 +18,16 @@ class SensorControl{
     SharpDistSensor sensor2(sensorPin2, medianFilterWindowSize);
     SharpDistSensor sensor3(sensorPin3, medianFilterWindowSize);
     SharpDistSensor sensor4(sensorPin4, medianFilterWindowSize);
-   // SharpDistSensor sensor5(sensorPin5, medianFilterWindowSize);
-   // SharpDistSensor sensor6(sensorPin6, medianFilterWindowSize);
-   // SharpDistSensor sensor7(sensorPin7, medianFilterWindowSize);
-   // SharpDistSensor sensor8(sensorPin8, medianFilterWindowSize);
+    SharpDistSensor sensor5(sensorPin5, medianFilterWindowSize);
+    SharpDistSensor sensor6(sensorPin6, medianFilterWindowSize);
+    SharpDistSensor sensor7(sensorPin7, medianFilterWindowSize);
+    SharpDistSensor sensor8(sensorPin8, medianFilterWindowSize);
     
     //Array to store distances 
-    uint16_t distances[4];
-    //uint16_t degrees[4]={0,45,90,135,180,225,270,315};
+    uint16_t distances[8];
     uint16_t OppR;
     int OppQ;
   public:
-    senseOpponent(uint16_t distances[4], uint16_t OppR, int OppQ);
+    senseOpponent(uint16_t distances[8]);
+    getLocation(uint16_t OppR, int OppQ);
 }
