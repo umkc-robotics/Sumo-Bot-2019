16f3bf3ba70fd55be04357015c29673340fc855a
Michael Essmyer <mde9rb@mail.umkc.edu>
2019-04-06 12:22:46 -0500

    Update SensorControl.cpp

--- ScrapController/SensorControl.cpp
+++ ScrapController/SensorControl.cpp
@@ -1,24 +1,28 @@
 #include "SensorControl.h"
 
-void SensorControl::senseOpponent(uint16_t& distances[4], uint16_t& OppR, int OppQ){
+void SensorControl::senseOpponent(uint16_t& distances[8]){
     // Get distance from sensor
   //unsigned int distance1 = sensor1.getDist();
   distances[0] = sensor1.getDist();
   distances[1] = sensor2.getDist();
   distances[2] = sensor3.getDist();
   distances[3] = sensor4.getDist();
-  //distances[4] = sensor5.getDist();
-  //distances[5] = sensor6.getDist();
-  //distances[6] = sensor7.getDist();
-  //distances[7] = sensor8.getDist();
-   // Print minimum IR
+  distances[4] = sensor5.getDist();
+  distances[5] = sensor6.getDist();
+  distances[6] = sensor7.getDist();
+  distances[7] = sensor8.getDist();
+  }
+
+int SensorControl::getLocation( uint16_t& OppR, int OppQ){
+  int degrees[8]={0,45,90,135,180,225,270,315};
+  // Print minimum IR
   uint16_t min_Dist = 10000;
   uint16_t low_Dist = 10000;
   uint16_t high_Dist = 10000;
   int min_IR = 10;
   int low_IR = 10;
   int high_IR = 10;
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 8; i++) {
       if(distances[i] < min_Dist){
         min_Dist = distances[i];
         min_IR = i;
@@ -31,7 +35,8 @@ void SensorControl::senseOpponent(uint16_t& distances[4], uint16_t& OppR, int Op
         low_IR = i;
         }
     }
-  OppR =  (min_Dist+low_Dist)/2;
+  OppR = (min_Dist+low_Dist)/2;
   OppQ = min_IR;
   Serial.println("Distance: ",OppR,"  Angle:" ,OppQ);
-  }
+  return degrees[OppQ];
+}
