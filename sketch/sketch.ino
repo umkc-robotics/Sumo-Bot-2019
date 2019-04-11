7b228a414b47f31556a43716fd3c20f7b64653b3
Michael Essmyer <mde9rb@mail.umkc.edu>
2019-04-10 22:39:37 -0500

    Update sketch.ino

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,40 +1,17 @@
 #include <Arduino.h>
-#include "Encoder.h"
-
-
-#define SENSOR_PIN A7
-
-int FRONT_LEFT_PIN_INTERRUPT = 37;
-int FRONT_LEFT_PIN_CHECKER = 36;
-
-int FRONT_LEFT_MOTOR_PWM = 12;
-int FRONT_LEFT_MOTOR_DIR = 13;
-
-Encoder encoderFL = Encoder(FRONT_LEFT_PIN_INTERRUPT, FRONT_LEFT_PIN_CHECKER);
-
+#include "Move.h"
+Move move = Move();
 
 
 void setup()
 {
   Serial.begin(9600);
-  initEncoders();
 }
 
   
 void loop()
 {
-  int count = encoderFL.getCount();
-  Serial.println(count);
+  move.performMovement();
 
 }
 
-
-
-  
-void initEncoders(){
-  // i.e (attachInterrupt( digitalPinToInterrupt(pin), ISR, mode); )
- attachInterrupt(digitalPinToInterrupt(FRONT_LEFT_PIN_INTERRUPT),checkEncoder,CHANGE);
- }
-void checkEncoder() {
- encoderFL.checkEncoder();
-}
