286ef6efed72e4d25c3a1e9e114b17b7322dcbbd
Ekrem <ekremkalabak@gmail.com>
2019-04-11 01:23:13 -0400

    Update LineSensor.h

--- libraries/LineSensor/LineSensor.h
+++ libraries/LineSensor/LineSensor.h
@@ -1,1 +1,19 @@
+#ifndef LineSensor_h
+#define LineSensor_h
 
+#include <Arduino.h>
+
+class LineSensor
+{
+	private:
+		byte pins[4]={2,3,4,5};
+		bool black[4];
+	public:
+		LineSensor(byte[4]);
+		bool CheckState();
+
+		void ReadOneSensor(int);
+		void ReadSensors();
+};
+
+#endif
